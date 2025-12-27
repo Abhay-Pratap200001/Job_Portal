122301765786adf5bc61fa34b57ccb90167091d3
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2025-12-27 10:58:43 +0530

    Created express server and connect with db and add asyncHandler to catch run time error and add error middleware

---
+++ Server/index.js
@@ -0,0 +1,44 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from 'cors'
+
+
+import { connectDB } from "./utils/dbConnect.js";
+import { errorHandler } from "./middleware/error.middleware.js";
+
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the request
+app.use(cookieParser());
+app.use(cors({
+    origin:'http//localhost:5173',
+    credentials:true
+}))
+
+
+//  Error handler 
+app.use(errorHandler);
+
+
+// Connect DB and start server
+connectDB().then(() => {
+    const server = app.listen(PORT, () => {
+      console.log(`✅ Server running at http://localhost:${PORT}`);
+    });
+
+    // Handle runtime errors gracefully
+    server.on("error", (error) => {
+      console.error("❌ Server Error:", error);
+      process.exit(1);
+    });
+  }).catch((error) => {
+    console.error("❌ MongoDB Connection Failed:", error);
+    process.exit(1);
+  });
+
+
